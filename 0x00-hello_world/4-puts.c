ec131e09fce1ab28b064e06a1fc8349eb6fb44e0
tahirioualid <62400517+tahirioualid@users.noreply.github.com>
2023-07-23 14:10:52 +0000

    up 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*
-This is a desc
-*/
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
