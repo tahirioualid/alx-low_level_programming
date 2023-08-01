fb72ee965e57a8bc8f815a3522a5de5f849d135c
tahirioualid <62400517+tahirioualid@users.noreply.github.com>
2023-08-01 01:04:08 +0000

    u^p

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,28 +7,27 @@
  */
 void puts_half(char *str)
 {
-  int len, n, i;
+	int len, n, i;
 
-  len = 0;
+	len = 0;
 
-  while (str[len] != '\0')
-  {
-    len++;
-  }
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-  if (len % 2 == 0)
-  {
-    for (i = len / 2; str[i] != '\0'; i++)
-    {
-      putchar(str[i]);
-    }
-  }
-  else if (len % 2)
-  {
-    for (n = (len - 1) / 2; n < len - 1; n++)
-    {
-      putchar(str[n + 1]);
-    }
-  }
-  putchar('\n');
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			 putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			 putchar(str[n + 1]);
+		}
+	}
+	putchar('\n');
 }
